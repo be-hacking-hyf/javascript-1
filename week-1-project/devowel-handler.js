d7593721a47ccd6c21f105f360e922e71c2f1e2c
akbel <colakakbel@gmail.com>
2019-10-10 10:42:00 +0200

    devowel handler (only english vowels and two others)

--- week-1-project/devowel-handler.js
+++ week-1-project/devowel-handler.js
@@ -12,20 +12,29 @@ the handler is already set up to:
 */
 
 function devowelHandler() {
-
   // read and process user input (this works, no need to change it!)
-  const toDevowel = document.getElementById('devowel-input').value;
+  const toDevowel = document.getElementById("devowel-input").value;
 
   // pass user input through core logic (write this! it doesn't work)
-  const devoweled = `remove all vowels from ${toDevowel}`;
 
+  function wegvowels() {
+    const vowels = ["a", "e", "o",'ø', "i", "u", "ü"];
+    return toDevowel
+      .split("")
+      .filter(function(wv) {
+        return vowels.indexOf(wv.toLowerCase()) == -1;
+      })
+      .join("");
+  }
+  const devoweled = wegvowels();
+  
   // report result to user (this works, no need to change it!)
-  const outputField = document.getElementById('devowel-output');
+  const outputField = document.getElementById("devowel-output");
   outputField.innerHTML = devoweled;
 
-  console.log('\n--- devowelHandler ---');
-  console.log('toDevowel:', typeof toDevowel, ',', toDevowel);
-  console.log('devoweled:', typeof devoweled, ',', devoweled);
-};
-const devowelButton = document.getElementById('devowel-button');
-devowelButton.addEventListener('click', devowelHandler);
+  console.log("\n--- devowelHandler ---");
+  console.log("toDevowel:", typeof toDevowel, ",", toDevowel);
+  console.log("devoweled:", typeof devoweled, ",", devoweled);
+}
+const devowelButton = document.getElementById("devowel-button");
+devowelButton.addEventListener("click", devowelHandler);
