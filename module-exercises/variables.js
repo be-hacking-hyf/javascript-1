7a39454aa2668d60a2040a710a364dfa82942ac9
Akadar.akku <akadar.akku121@gmail.com>
2019-10-11 11:17:13 +0200

    Edited

--- module-exercises/variables.js
+++ module-exercises/variables.js
@@ -82,7 +82,10 @@ function threeVariableSwap1() {
   let temp = '';
 
   // can be done in 4 lines
-
+  temp = a;
+  a = b;
+  b = c;
+  c = temp;
 
   console.assert(a === "a", "a should store 'a'");
   console.assert(b === "b", "b should store 'b'");
@@ -96,7 +99,10 @@ function threeVariableSwap2() {
   let temp = '';
 
   // can be done in 4 lines
-
+  temp = a;
+  a = c;
+  c = b;
+  b = temp;
 
   console.assert(a === "a", "a should store 'a'");
   console.assert(b === "b", "b should store 'b'");
