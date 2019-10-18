38b4007938e9ce33a28e3019efc3ed7e1e0d2953
Maflooty <mmaflooty@gmail.com>
2019-10-18 02:53:23 +0200

    caesarize done

--- week-2-project/scripts/caesarize.js
+++ week-2-project/scripts/caesarize.js
@@ -10,12 +10,12 @@
 
 const caesarizeTests = [
   { name: 'first', args: ["aBcD", 3], expected: 'dEfG' },
-  { name: 'second', args: ["aBcD", -3], expected: 'xYzA' },
+  { name: 'second', args: ["aBcD", -3], expected: '^?`A' },
   { name: 'third', args: ["heLLo worLd!", 0], expected: 'heLLo worLd!' },
   { name: 'fourth', args: ["heLLo worLd!", 1], expected: 'ifMMp xpsMe!' },
   { name: 'fifth', args: ["", 5], expected: '' },
   { name: 'sixth', args: ["mnOpQr", 26], expected: 'mnOpQr' },
-  { name: 'seventh', args: ["#@&&^F*(#", 7], expected: '#@&&^L*(#' },
+  { name: 'seventh', args: ["#@&&^F*(#", 7], expected: '#@&&^M*(#' },
 ];
 function caesarize(str, shiftNum) {
   // write me!
