a6d30936e155178657b2c2769997e8b0691e8e5c
serhat <serhat-sumer@hotmail.com>
2019-10-18 17:26:04 +0200

    caesarize.js created

--- week-2-project/scripts/caesarize.js
+++ week-2-project/scripts/caesarize.js
@@ -1,9 +1,6 @@
 /* Caesar Cipher
-
   this is a simple encoding algorithm that replaces letters in a message with a new letter
-
   the new letter is determined by shifting N spaces across the alphabet
-
   for example, caesarize("A", 3) will return : "D"
   because "D" is three letters past "A".
 */
@@ -15,12 +12,42 @@ const caesarizeTests = [
   { name: 'fourth', args: ["heLLo worLd!", 1], expected: 'ifMMp xpsMe!' },
   { name: 'fifth', args: ["", 5], expected: '' },
   { name: 'sixth', args: ["mnOpQr", 26], expected: 'mnOpQr' },
-  { name: 'seventh', args: ["#@&&^F*(#", 7], expected: '#@&&^L*(#' },
+  { name: 'seventh', args: ["#@&&^F*(#", 7], expected: '#@&&^M*(#' },
 ];
 function caesarize(str, shiftNum) {
-  // write me!
-}
-evaluate(caesarize, caesarizeTests);
+ 
+    var input = str;
+    var str=input.split('');
+    
+         
+      for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charCodeAt();
+            if ((str[i]>=97)&&(str[i]<=122)){
+              str[i]+=shiftNum;
+                if (str[i]>122){
+                  str[i]=str[i]-26;
+                  }
+                  else if(str[i]<97){
+                    str[i]=str[i]+26;
+                  }
+                  str[i] = String.fromCharCode(str[i]);
+                }
+            else if((str[i]>=65)&&(str[i]<=90)){
+              str[i]+=shiftNum;
+                 if (str[i]>90){
+                  str[i]=str[i]-26;
+                  }
+                   else if (str[i]<65){
+                    str[i]=str[i]+26;
+                  }
+                  str[i] = String.fromCharCode(str[i]);
+                }
+          
+      }
+      return str.join('');
+    }
+    
+    evaluate(caesarize, caesarizeTests);
 
 
 function caesarizeHandler() {
@@ -36,7 +63,7 @@ function caesarizeHandler() {
 
 
   // pass user input through core logic (this works!  no need to change it)
-  const caesarized = caesarize(strToCaesarize);
+  const caesarized = caesarize(strToCaesarize, shiftNumber);
 
   // report result to user (this works, no need to change it!)
   const outputField = document.getElementById('caesarize-output');
