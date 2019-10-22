23d37acf5ccb70ba9e49be6e1e529ed2641d27a2
Mustapha <53609690+maflooty@users.noreply.github.com>
2019-10-22 21:07:02 +0200

    Create reverse-chunk.js

---
+++ week-3-project/logic/reverse-chunk.js
@@ -0,0 +1,18 @@
+/* reverse a string
+  reverse the order of words in a string.
+  a word is anything with spaces on either side of it
+  the spaces, tabs and newlines stay put
+*/
+
+
+const reverseChunkTests = [
+  { name: 'first', args: [' plug (play!)'], expected: ' (play!) plug' },
+  { name: 'second', args: [' --   - '], expected: ' -   -- ' },
+  { name: 'third', args: ['12 34 '], expected: '34 12 ' },
+  { name: 'fourth', args: ['const x = null; '], expected: 'null = x const ' },
+  { name: 'fifth', args: ['   e e '], expected: '   e e ' },
+];
+function reverseChunk(str) {
+  // write me!
+}
+evaluate(reverseChunk, reverseChunkTests);
