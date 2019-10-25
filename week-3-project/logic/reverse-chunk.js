7ae61b975cdefb17b5bf2f740987a499c410dae7
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2019-10-25 12:04:55 +0200

    ;

--- week-3-project/logic/reverse-chunk.js
+++ week-3-project/logic/reverse-chunk.js
@@ -12,7 +12,7 @@ const reverseChunkTests = [
   { name: 'first', args: [' plug (play!)'], expected: ' (play!) plug' },
   { name: 'second', args: [' --   - '], expected: ' -   -- ' },
   { name: 'third', args: ['12 34 '], expected: '34 12 ' },
-  { name: 'fourth', args: ['const x = null; '], expected: 'null = x const; ' },
+  { name: 'fourth', args: ['const x = null; '], expected: 'null; = x const ' },
   { name: 'fifth', args: ['   e e '], expected: '   e e ' },
 ];
 function reverseChunk(str) {
